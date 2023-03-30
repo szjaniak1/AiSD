cbe1264419976c9aa5d495b7fa38c82e7bfab64d
szjaniak <szjaniak1@gmail.com>
2023-03-30 20:01:09 +0200

    modified insertion sort program to count number of shifts and comparisons

--- lista02/zad1/sortingAlg/insertionSort.c
+++ lista02/zad1/sortingAlg/insertionSort.c
@@ -1,6 +1,33 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+static int numberOfComparisons = 0;
+static int numberOfShifts = 0;
+
+void printArray(int *tab, int size);
+
+bool isOk(int j, int *tab, int key)
+{
+	numberOfComparisons++;
+	if (tab[j] > key)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool shift(int j, int i, int *tab, int key)
+{
+	if (j + 1 == i)
+	{
+		return false;
+	}
+	tab[j + 1] = key;
+	numberOfShifts++;
+	return true;
+}
 
 void insertionSort(int *tab, int size)
 {
@@ -10,13 +37,23 @@ void insertionSort(int *tab, int size)
 	{
 		key = tab[i];
 		j = i - 1;
+		
 
-		while (j >= 0 && tab[j] > key)
+		while (j >= 0 && isOk(j, tab, key))
 		{
 			tab[j + 1] = tab[j];
 			j--;
+			numberOfShifts++;
+			if (size < 40)
+			{
+				printArray(tab, size);
+			}
 		}
-		tab[j + 1] = key;
+
+		if (shift(j, i, tab, key) && size < 40)
+		{
+			printArray(tab, size);
+		}
 	}
 }
 
@@ -27,6 +64,18 @@ void printArray(int *tab, int size)
 		printf("%02d ", *(tab + i));
 	}
 	printf("%c", '\n');
+}
+
+bool isSorted(int *tab, int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		if (*(tab + i - 1) > *(tab + i))
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 int* takeInput(int argc)
@@ -45,10 +94,25 @@ int main()
 {
 	int size;
        	scanf("%d", &size);	
-	int *arr = takeInput(size);;
-	printArray(arr, size);
+	int *arr = takeInput(size);
+
+	if (size < 40)
+	{
+		printArray(arr, size);
+	}
+
 	insertionSort(arr, size);
-	printArray(arr, size);
+	
+	printf("\ntotal number of comparisons: %d, total number of shifts: %d\n", numberOfComparisons, numberOfShifts);
+	
+	if (isSorted(arr, size))
+	{
+		printf("array is sorted!");
+	}
+	else
+	{
+		printf("array is NOT sorted!");
+	}
 
 	return 0;
 }
