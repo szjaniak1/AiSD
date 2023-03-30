42617e96b9e63fa2a3374c594c1bb06c4f85178e
szjaniak <szjaniak1@gmail.com>
2023-03-30 18:59:41 +0200

    descending and ascending number generators

---
+++ lista02/zad1/generators/ascendingNumberGenerator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <bits/stdc++.h>
+
+void getNumbers(const int size)
+{
+	std::random_device rd;
+	std::seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+	std::uniform_int_distribution<int> range(0, 2 * size - 1);
+	std::mt19937 mt{ ss };
+	
+	std::vector<int> numbers;
+	numbers.reserve(2 * size - 1);
+	
+	for (int i = 0; i < size; i++)
+	{
+		numbers.push_back( range(mt) );
+	}
+	
+	std::sort(numbers.begin(), numbers.end());
+
+	for (int num : numbers)
+	{
+		std::cout << num << " ";
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	const int size = atoi(argv[1]);
+	std::cout << size << " ";
+	getNumbers(size);
+
+	return 0;
+}
