15fb13940cfb71ef01db9917387acab40a436e2b
szjaniak <szjaniak1@gmail.com>
2023-04-02 20:36:16 +0200

    modified script to get better data structure in file

--- lista02/zad2/script.sh
+++ lista02/zad2/script.sh
@@ -2,53 +2,43 @@
 
 rm ./data/sortingAlgResults.csv
 declare -a algorithms=("./sortingAlg/qS.o" "./sortingAlg/mS.o" "./sortingAlg/iS.o" "./sortingAlg/dPQS.o" "./sortingAlg/cS.o")
+declare -a algNames=("quicksort" "mergesort" "insertionsort" "dualpivotquicksort" "customsort")
 datafile="./data/sortingAlgResults.csv"
+declare -i i=1
 
-for n in {10..200..10}
-do
-	echo -n ";$n;$n" >> $datafile
-done
-
-echo "" >> $datafile
+echo "type;n;comp;shifts" >> $datafile
 
 for alg in ${algorithms[@]}
 do
-	for k in 1 10 100
+	for n in {10..200..10}
 	do
-		for k1 in $(seq 1 $k)
+		for k in 1 10 100
 		do
-			echo -n "$k;" >> $datafile
-			for n in {10..200..10}
+			for k1 in $(seq 1 $k)
 			do
-				echo -n "$(../zad1/generators/rNG.o $n | $alg);" >> $datafile
+				echo "${algNames[$i]};$n;$(../zad1/generators/rNG.o $n | $alg);" >> $datafile
 			done
-			echo "" >> $datafile
 		done
 	done
+	i=$(( $i + 1))
 done
 
-for n in {1000..20000..1000}
-do
-	echo -n ";$n;$n" >>$datafile
-done
-
-echo "" >> $datafile
+i=0
 
 for alg in ${algorithms[@]}
 do
 	if [ $alg != "./sortingAlg/iS.o" ] 
 	then
-		for k in 1 10 100
+		for n in {1000..20000..1000}
 		do
-			for k1 in $(seq 1 $k)
+			for k in 1 10 100
 			do
-				echo -n "$k;" >> $datafile
-				for n in {1000..20000..1000}
+				for k1 in $(seq 1 $k)
 				do
-					echo -n "$(../zad1/generators/rNG.o $n | $alg);" >> $datafile
+					echo "${algNames[$i]};$n;$(../zad1/generators/rNG.o $n | $alg);" >> $datafile
 				done
-				echo "" >> $datafile
 			done
 		done
 	fi
+	i=$(( $i + 1))
 done
