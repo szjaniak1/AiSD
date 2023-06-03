8d8153c7893fd47394dd90e8ff6c42ad56004c03
szjaniak1 <szjaniak1@gmail.com>
2023-06-03 05:08:09 +0200

    cleanup

--- lista04/main.c
+++ lista04/main.c
@@ -21,32 +21,39 @@ int32_t* take_input(const size_t size)
 // n = size
 int main(void)
 {
-    const size_t size = 50;
-    const int32_t max_num = 2 * size - 1;
+    size_t size;
+    int32_t value;
+    scanf("%zu", &size);
+    const int32_t max_num = 2 * (int32_t)size - 1;
     int32_t *insert_arr = take_input(size);
     int32_t *delete_arr = take_input(size);
 
     init_traces(size);
 
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
 
     Node *root = NULL;
-    root = insert(root, arr[0]);
+    root = insert(root, insert_arr[0]);
 
     size_t i;
     for (i = 1; i < size; i++)
     {
         value = insert_arr[i];
         insert(root, value);
-        print_bst(root, 0, "-");
+        //print_bst(root, 0, '-');
     }
+
+    print_bst(root, 0, '_');
+    printf("\n\n\n\n");
 
     for (i = 0; i < size; i++)
     {
         value = delete_arr[i];
         delete(root, value);
-        print_bst(root, 0, '_');
+        //print_bst(root, 0, '_');
     }
+
+    print_bst(root, 0, '_');
 
     free(insert_arr);
     free(delete_arr);
